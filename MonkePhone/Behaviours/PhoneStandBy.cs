c35e3aeae40505d5af905847a09a427d3bc06d10
E14O <Stan19marvel@icloud.com>
2026-01-11 12:24:25 +0000

    Update PhoneStandBy

--- MonkePhone/Behaviours/PhoneStandBy.cs
+++ MonkePhone/Behaviours/PhoneStandBy.cs
@@ -11,68 +11,68 @@ namespace MonkePhone.Behaviours
     public class PhoneStandBy : MonoBehaviour
     {
         private bool _MonkeGramOpen = false;
+        private bool _GUIShow = false;
 
         /// <summary>
         /// This is for testing the post system and other systems on computer. (It does not send the photo to a server it only works if a webhook is set)
         /// </summary>
-        /* public void OnGUI()
+        public void OnGUI()
         {
-            if (GUI.Button(new Rect(128f, 210f, 150f, 35f), "HomeButton")) PhoneManager.Instance.SetHome();
-
-            if (GUI.Button(new Rect(128f, 170f, 150f, 35f), "PowerButton")) PhoneManager.Instance.TogglePower();
-
-            if (GUI.Button(new Rect(128f, 250f, 150f, 35f), "ActionButton")) PhoneLockScreen.SetActionButton(); 
-
-            if (GUI.Button(new Rect(128f, 290f, 150f, 35f), "MonkeGram"))
+            if (_GUIShow)
             {
-                if (!_MonkeGramOpen)
+                if (GUI.Button(new Rect(128f, 210f, 150f, 35f), "HomeButton")) PhoneManager.Instance.SetHome();
+
+                if (GUI.Button(new Rect(128f, 170f, 150f, 35f), "PowerButton")) PhoneManager.Instance.TogglePower();
+
+                if (GUI.Button(new Rect(128f, 250f, 150f, 35f), "ActionButton")) PhoneLockScreen.SetActionButton();
+
+                if (GUI.Button(new Rect(128f, 290f, 150f, 35f), "MonkeGram"))
                 {
-                    App("Open", "MonkeGram");
-                    _MonkeGramOpen = true;
+                    if (!_MonkeGramOpen)
+                    {
+                        App("Open", "MonkeGram");
+                        _MonkeGramOpen = true;
+                    }
+                    else
+                    {
+                        App("Close", "MonkeGram");
+                        _MonkeGramOpen = false;
+                    }
                 }
-                else
+
+
+                bool post = GUI.Button(new Rect(128f, 130f, 150f, 35f), "Post Sample");
+                if (post)
                 {
-                    App("Close", "MonkeGram");
-                    _MonkeGramOpen = false;
+                    string _Folder = PhoneManager.Instance.PhotosPath;
+
+                    var _Files = Directory.GetFiles(_Folder, "*.png");
+
+                    if (_Files.Length == 0) return;
+
+                    string _RandomFile = _Files[Random.Range(0, _Files.Length)];
+
+                    byte[] bytes = File.ReadAllBytes(_RandomFile);
+
+                    Photo photo = new()
+                    {
+                        Name = Path.GetFileName(_RandomFile),
+                        Bytes = bytes,
+                        Summary = $"{GorillaComputer.instance.currentName} posted a photo:"
+                    };
+
+                    PhoneManager.Instance.GetApp<GalleryApp>().RelativePhotos.Add(photo);
+                    PhoneManager.Instance.GetApp<GalleryApp>().SendWebhook(photo.Summary, photo.Name, photo.Bytes);
+                    PhoneManager.Instance.ApplyWallpaper(false, _RandomFile);
                 }
             }
-
-
-            bool post = GUI.Button(new Rect(128f, 130f, 150f, 35f), "Post Sample");
-            if (post)
-            {
-                string _Folder = PhoneManager.Instance.PhotosPath;
-
-                var _Files = Directory.GetFiles(_Folder, "*.png");
-
-                if (_Files.Length == 0) return;
-
-                string _RandomFile = _Files[Random.Range(0, _Files.Length)];
-
-                byte[] bytes = File.ReadAllBytes(_RandomFile);
-
-                Photo photo = new()
-                {
-                    Name = Path.GetFileName(_RandomFile),
-                    Bytes = bytes,
-                    Summary = $"{GorillaComputer.instance.currentName} posted a photo:"
-                };
-
-                PhoneManager.Instance.GetApp<GalleryApp>().RelativePhotos.Add(photo);
-                PhoneManager.Instance.GetApp<GalleryApp>().SendWebhook(photo.Summary, photo.Name, photo.Bytes);
-                PhoneManager.Instance.ApplyWallpaper(false, _RandomFile);
-            }
-        } */
+        } 
 
         private void Update()
         {
             if (UnityEngine.InputSystem.Keyboard.current.tabKey.wasPressedThisFrame)
             {
-
-            }
-            else
-            {
-
+                _GUIShow = !_GUIShow;
             }
         }
     }
