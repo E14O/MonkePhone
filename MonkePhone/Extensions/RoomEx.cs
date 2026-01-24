dd51f9c82288f7f69fd2e942bcec20f49a306fe8
Spankypluh <liamkasia2010@gmail.com>
2026-01-24 16:27:10 -0500

    More Modes

--- MonkePhone/Extensions/RoomEx.cs
+++ MonkePhone/Extensions/RoomEx.cs
@@ -22,15 +22,30 @@ namespace MonkePhone.Extensions
 			if (room.CustomProperties.TryGetValue("gameMode", out object modeObj) && modeObj != null)
 			{
 				string modeStr = modeObj.ToString();
-
-				if (modeStr.Contains("MODDED_Casual"))
+				if (modeStr.Contains("MODDED_SuperCasual"))
+					return "(M)(S) Casual";
+				else if (modeStr.Contains("MODDED_SuperInfect"))
+					return "(M)(S) Infection";
+				else if (modeStr.Contains("MODDED_Casual"))
 					return "(M) Casual";
 				else if (modeStr.Contains("MODDED_Infection"))
 					return "(M) Infection";
+				else if (modeStr.Contains("MODDED_Paintbrawl"))
+					return "(M) Paintbrawl";
+				else if (modeStr.Contains("MODDED_Guardian"))
+					return "(M) Guardian";
 				else if (modeStr.Contains("Casual"))
 					return "Casual";
 				else if (modeStr.Contains("Infection"))
 					return "Infection";
+				else if (modeStr.Contains("Paintbrawl"))
+					return "Paintbrawl";
+				else if (modeStr.Contains("Guardian"))
+					return "Guardian";
+				else if (modeStr.Contains("SuperInfect"))
+					return "(S) Infection";
+				else if (modeStr.Contains("SuperCasual"))
+					return "(S) Casual";
 				else
 					return modeStr;
 			}
